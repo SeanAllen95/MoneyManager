776a9aae1555c4c57665ba0171f32630b58f31c9
Sean <scallen11@btinternet.com>
2023-05-02 14:12:34 +0100

    working on user

--- controllers/merchant_controller.py
+++ controllers/merchant_controller.py
@@ -14,12 +14,15 @@ def merchant():
     merchant = merchant_repository.select_all()
     return render_template("merchant/index.html", merchant = merchant)
 
-# @merchant_blueprint.route('/merchants')
-
 @merchant_blueprint.route("/merchant/new")
 def new_merchant():
     merchants = merchant_repository.select_all()
     return render_template("merchant/new_merchant.html", merchants = merchants)
+
+@merchant_blueprint.route("/merchant/new_transaction")
+def new_transaction():
+    merchants = merchant_repository.select_all()
+    return render_template("merchant/new_transaction.html", merchants = merchants)
 
 @merchant_blueprint.route("/merchant",  methods=['POST'])
 def create_merchant():
@@ -33,10 +36,25 @@ def create_merchant():
 @merchant_blueprint.route("/merchant/edit/<id>", methods=['GET'])
 def edit_merchant(id):
     merchant = merchant_repository.select(id)
+    merchant_repository.save(merchant)
     return render_template("merchant/edit.html", merchant = merchant)
+
+@merchant_blueprint.route("/merchant/<id>", methods=['POST'])
+def update_method(id):
+    name = request.form['name']
+    category = request.form['category']
+    amount = request.form['amount']
+    merchant = Merchant(name, category, amount, id)
+    merchant_repository.update(merchant)
+    return redirect('/merchant')
 
 
 @merchant_blueprint.route("/merchant/view/<id>", methods=['GET'])
 def view_merchant(id):
     merchant = merchant_repository.select(id)
     return render_template("merchant/view.html", merchant = merchant)
+
+@merchant_blueprint.route("/merchant/<id>/delete", methods=['POST'])
+def delete_merchant(id):
+    merchant_repository.delete(id)
+    return redirect('/merchant')
--- repositories/merchant_repository.py
+++ repositories/merchant_repository.py
@@ -36,3 +36,12 @@ def delete_all():
     sql = "DELETE FROM merchants"
     run_sql(sql)
 
+def delete(id):
+    sql = "DELETE FROM merchants WHERE id = (%s)"
+    values = [id]
+    run_sql(sql, values)
+
+def update(merchant):
+    sql = "UPDATE merchants SET (name, category, amount) = (%s, %s, %s) WHERE id = %s"
+    values = [merchant.name, merchant.category, merchant.amount]
+    run_sql(sql, values)
